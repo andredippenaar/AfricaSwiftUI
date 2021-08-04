c19a87af91068899ba75f6962b93288b7241f9ce
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-08-04 09:44:18 +0200

    Initial Commit

---
+++ AfricaSwiftUI/AfricaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaSwiftUIApp.swift
+//  AfricaSwiftUI
+//
+//  Created by Andre Dippenaar on 2021/08/04.
+//
+
+import SwiftUI
+
+@main
+struct AfricaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
