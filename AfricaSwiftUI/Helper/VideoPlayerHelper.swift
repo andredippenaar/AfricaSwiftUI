40652171c401cf415e6612e189a293af37033b8c
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-08-25 09:50:49 +0200

    Complete Video Player functionality

---
+++ AfricaSwiftUI/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerHelper.swift
+//  VideoPlayerHelper
+//
+//  Created by Andre Dippenaar on 2021/08/25.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(filename: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: filename, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: filename, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
